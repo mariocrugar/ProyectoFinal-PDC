95af6f8be271507febb3356e5fb8ff2dd0f3824e
mariocrugar <47091822+mariocrugar@users.noreply.github.com>
2019-12-09 15:15:19 -0600

    Create pipeline.sh
    Template del código para la ejecución de los demás procesos

---
+++ sh/pipeline.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Ejecución para la exptracci{on y proceso de limpiado de la base de datos.
+
+# Descarga de la Base de datos
+# Primero, asignar los permisos necesarios para la ejecución de los comandos necesarios.
+chmod +x ./sh/XXXXXXX.sh
+
+./sh/XXXXXXXX.sh
+
+# Creación del usuario en postgres y la base de datos
+
+chmod +x ./sh/YYYYYYYY.sh
+
+./sh/YYYYYYYY.sh
+
+# Procesamiento de los datos
+
+chmod +x ./sh/process_data.sh
+
+./sh/WWWWWWW.sh
