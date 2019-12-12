1fc0f7f894e4ff5bdecac71949fd1a3fcbbddeb9
Mario Rodríguez <42506538+shimanteko@users.noreply.github.com>
2019-12-11 23:52:54 -0600

    Update to_semantic.sql

--- realsuk/sql/to_semantic.sql
+++ realsuk/sql/to_semantic.sql
@@ -1,1 +1,31 @@
+create table if not exists semantic.entities as (
+    select distinct
+      uid,
+      postcode,
+      propert_type,
+      old_new,
+      street,
+      locality,
+      town_city,
+      district,
+      county
+      from
+          cleaned.hmlr_prices
+  );  
+ 
+ 
+ create table if not exists semantic.events as (
+    select distinct
+      uid,
+      price,
+      date_of_transfer,
+      postcode,
+      duration,
+      ppd_category,
+	  record_status
+      from
+          cleaned.hmlr_prices
+  );  
 
+create index semantic_entities_property on semantic.entities(uid);
+create index semantic_entities_transaction on semantic.events(uid);  
